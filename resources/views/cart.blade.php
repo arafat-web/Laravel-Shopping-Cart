b17b1118806b03011c7676874f88fa3843bc30d5
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-08-24 13:07:13 +0600

    cart design updated

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,7 +5,7 @@
     <main class="my-8">
         <div class="container px-6 mx-auto">
             <div class="flex justify-center my-6">
-                <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+                <div class="flex flex-col w-full block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 md:w-4/5 lg:w-4/5">
                     @if ($message = Session::get('success'))
                         <div class="p-4 mb-3 bg-green-400 rounded">
                             <p class="text-green-800">{{ $message }}</p>
@@ -15,74 +15,74 @@
                     <div class="flex-1">
                         <table class="w-full text-sm lg:text-base" cellspacing="0">
                             <thead>
-                            <tr class="h-12 uppercase">
-                                <th class="hidden md:table-cell"></th>
-                                <th class="text-left">Name</th>
-                                <th class="pl-5 text-left lg:text-right lg:pl-0">
-                                    <span class="lg:hidden" title="Quantity">Qtd</span>
-                                    <span class="hidden lg:inline">Quantity</span>
-                                </th>
-                                <th class="hidden text-right md:table-cell"> price</th>
-                                <th class="hidden text-right md:table-cell"> Remove </th>
-                            </tr>
+                                <tr class="h-12 uppercase">
+                                    <th class="hidden md:table-cell"></th>
+                                    <th class="text-left">Name</th>
+                                    <th class="pl-5 text-left lg:text-right lg:pl-0">
+                                        <span class="lg:hidden" title="Quantity">Qtd</span>
+                                        <span class="hidden lg:inline">Quantity</span>
+                                    </th>
+                                    <th class="hidden text-right md:table-cell"> price</th>
+                                    <th class="hidden text-right md:table-cell"> Remove </th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @foreach ($cartItems as $item)
-                                <tr>
-                                    <td class="hidden pb-4 md:table-cell">
-                                        <a href="#">
-                                            <img src="{{ $item->attributes->image }}" class="w-20 rounded" alt="Thumbnail">
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="#">
-                                            <p class="mb-2 md:ml-4">{{ $item->name }}</p>
-
-                                        </a>
-                                    </td>
-                                    <td class="justify-center mt-6 md:justify-end md:flex">
-                                        <div class="h-10 w-28">
-                                            <div class="relative flex flex-row w-full h-8">
-
-                                                <form action="{{ route('cart.update') }}" method="POST">
-                                                    @csrf
-                                                    <input type="hidden" name="id" value="{{ $item->id}}" >
-                                                    <input type="number" name="quantity" value="{{ $item->quantity }}"
-                                                           class="w-6 text-center bg-gray-300" />
-                                                    <button type="submit" class="px-2 pb-2 ml-2 text-white bg-blue-500">update</button>
-                                                </form>
+                                @foreach ($cartItems as $item)
+                                    <tr>
+                                        <td class="hidden pb-4 md:table-cell">
+                                            <a href="#">
+                                                <img src="{{ $item->attributes->image }}" class="w-20 rounded"
+                                                    alt="Thumbnail">
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a href="#">
+                                                <p class="mb-2 capitalize  md:ml-4">{{ $item->name }}</p>
+                                            </a>
+                                        </td>
+                                        <td class="justify-center mt-6 md:justify-end md:flex">
+                                            <div class="h-10 w-28">
+                                                <div class="relative flex flex-row w-full h-8">
+                                                    <form action="{{ route('cart.update') }}" method="POST">
+                                                        @csrf
+                                                        <input type="hidden" name="id" value="{{ $item->id }}">
+                                                        <input type="number" name="quantity" value="{{ $item->quantity }}"
+                                                            class="appearance-none w-12 bg-gray-200 text-gray-700 border border-gray-200 rounded py-1 border-b-4 border-blue-700 px-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+                                                        <button type="submit"
+                                                            class="text-xs bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">✓</button>
+                                                    </form>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td class="hidden text-right md:table-cell">
-                                <span class="text-sm font-medium lg:text-base">
-                                    ${{ $item->price }}
-                                </span>
-                                    </td>
-                                    <td class="hidden text-right md:table-cell">
-                                        <form action="{{ route('cart.remove') }}" method="POST">
-                                            @csrf
-                                            <input type="hidden" value="{{ $item->id }}" name="id">
-                                            <button class="px-4 py-2 text-white bg-red-600">x</button>
-                                        </form>
+                                        </td>
+                                        <td class="hidden text-right md:table-cell">
+                                            <span class="text-sm font-medium lg:text-base">
+                                                ${{ $item->price }}
+                                            </span>
+                                        </td>
+                                        <td class="hidden text-right md:table-cell">
+                                            <form action="{{ route('cart.remove') }}" method="POST">
+                                                @csrf
+                                                <input type="hidden" value="{{ $item->id }}" name="id">
+                                                <button
+                                                    class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded text-red-100 text-xs">❌</button>
+                                            </form>
 
-                                    </td>
-                                </tr>
-                            @endforeach
-
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                         <div>
                             Total: ${{ Cart::getTotal() }}
                         </div>
                         <div>
-                            <form action="{{ route('cart.clear') }}" method="POST">
+                            <form class="flex justify-end" action="{{ route('cart.clear') }}" method="POST">
                                 @csrf
-                                <button class="px-6 py-2 text-red-800 bg-red-300">Remove All Cart</button>
+                                <button
+                                    class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded text-red-100 text-xs">Remove
+                                    All</button>
                             </form>
                         </div>
-
-
                     </div>
                 </div>
             </div>
