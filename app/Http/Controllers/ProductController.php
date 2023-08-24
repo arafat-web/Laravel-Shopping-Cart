26cf95ece4bfc57669dbe027f3d2f793ced87bc7
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-08-24 13:06:57 +0600

    new pages added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,20 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function index()
+    {
+        $products = Product::take(8)->orderBy('id', 'desc')->get();
+        return view('home', compact('products'));
+    }
     public function productList()
     {
-        $products = Product::all();
+        $products = Product::orderBy('id', 'desc')->get();
         return view('products', compact('products'));
+    }
+
+    public function contact()
+    {
+        return view('contact');
     }
 }
